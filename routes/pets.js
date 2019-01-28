fba38789eb322b3e5923da9d24cebb0e958c29ed
Jayce Azua <jayce.azua@gmail.com>
2019-01-27 18:04:27 -0800

    s3 file uploads

--- routes/pets.js
+++ routes/pets.js
@@ -42,38 +42,34 @@ petsRouter.get('/pets/new', (req, res) => {
 
 // CREATE PET
 petsRouter.post('/pets', upload.single('avatar'), (req, res, next) => {
-  
-  let pet = new Pet(req.body);
-  // console.log(pet);
-  // next();
-  // res.redirect('/')
-  // pet.save(function (err) {
-  //   if (req.file) {
-  //     client.upload(req.file.path, {}, function (err, versions, meta) {
-  //       if (err) {
-  //         return res.status(400).send({
-  //           err: err
-  //         })
-  //       };
+  var pet = new Pet(req.body);
+  pet.save(function (err) {
+    if (req.file) {
+      client.upload(req.file.path, {}, function (err, versions, meta) {
+        if (err) {
+          return res.status(400).send({
+            err: err
+          })
+        };
 
-  //       versions.forEach(function (image) {
-  //         var urlArray = image.url.split('-');
-  //         urlArray.pop();
-  //         var url = urlArray.join('-');
-  //         pet.avatarUrl = url;
-  //         pet.save();
-  //       });
+        versions.forEach(function (image) {
+          var urlArray = image.url.split('-');
+          urlArray.pop();
+          var url = urlArray.join('-');
+          pet.avatarUrl = url;
+          pet.save();
+        });
 
-  //       res.send({
-  //         pet: pet
-  //       });
-  //     });
-  //   } else {
-  //     res.send({
-  //       pet: pet
-  //     });
-  //   }
-  // })
+        res.send({
+          pet: pet
+        });
+      });
+    } else {
+      res.send({
+        pet: pet
+      });
+    }
+  })
 })
 
 // EDIT PET
